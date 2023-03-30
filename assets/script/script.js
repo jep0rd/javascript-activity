d07d6eedff5dc72a19430638195453475fc8f06c
Geferd Gonzales <geferdgonzales1@gmail.com>
2023-03-30 17:45:42 +0800

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,22 @@
+let ml = document.getElementById("mLiter").value;
+
+// let lt = document.getElementById("liter").value;
+// let gal = document.getElementById("gallon").value;
+document.getElementById("liter").innerText = 1000;
+document.getElementById("gallon").innerText = 0.264172;
+
+function liter(x)
+{
+  return parseFloat(x) / 1000;
+}
+function gallon(y)
+{
+  return parseFloat(y) / 3785.41;
+}
+
+function display()
+{
+  let ml = document.getElementById("mLiter").value;
+  document.getElementById("liter").innerText = liter(ml);
+  document.getElementById("gallon").innerText = gallon(ml);
+}
